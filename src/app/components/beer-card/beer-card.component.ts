7278d8b1eeed278367d3a0ef9549204244c0b11f
Joaquin Ferreira Garcia <jferreig@ext.unicaja.es>
2025-05-19 15:57:36 +0200

    beer-card and list

--- src/app/components/beer-card/beer-card.component.ts
+++ src/app/components/beer-card/beer-card.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, Signal, input, output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { Beer } from '../../models/beer.model';
 
 @Component({
   selector: 'app-beer-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CardModule, ButtonModule],
   templateUrl: './beer-card.component.html',
-  styleUrl: './beer-card.component.scss'
+  styleUrl: './beer-card.component.scss',
 })
 export class BeerCardComponent {
+  readonly beer = input<Beer | undefined>(undefined);
+  readonly imageUrl = input<string | undefined>(undefined);
 
+  readonly onViewClick = output<Beer | undefined>();
+  handleClick(): void {
+    this.onViewClick.emit(this.beer());
+  }
 }
--- src/app/pages/beer-list/beer-list.component.ts
+++ src/app/pages/beer-list/beer-list.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BeerService } from '../../services/beer.service';
+import { Beer } from '../../models/beer.model';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { BeerCardComponent } from '../../components/beer-card/beer-card.component';
 
 @Component({
   selector: 'app-beer-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, BeerCardComponent],
   templateUrl: './beer-list.component.html',
-  styleUrl: './beer-list.component.scss'
+  styleUrl: './beer-list.component.scss',
 })
 export class BeerListComponent {
+  private readonly beerService = inject(BeerService);
 
+  readonly beers = toSignal(this.beerService.getBeersByPage(1), { initialValue: [] });
+
+  readonly selectedBeer = signal<Beer | null>(null);
+
+  handleViewBeer = (beer: Beer) => {
+    this.selectedBeer.set(beer);
+    console.log('Selected Beer:', beer);
+  };
 }
